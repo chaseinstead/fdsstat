989dbbc6a19a6700d4df5ba28ab1351bab586ec4
nportfolio <44119797+chaseinstead@users.noreply.github.com>
2019-05-03 18:14:22 +0200

    generalized http error handling

--- get_json.py
+++ get_json.py
@@ -7,7 +7,9 @@ def get_full_json(url):
 
 	a = requests.get(url)
 
-	if a.status_code == 404:
+	if a.status_code != 200:
+		print(a.status_code)
+
 		with open('data.json', 'a') as outfile:
 			json.dump(data, outfile)
 
@@ -27,23 +29,12 @@ def generate_next_url(url):
 		full_url = url + str(number)
 
 		number += 50
-
-#		if number >= 1000:
-#			raise StopIteration()
-
+		
+		print("adding", full_url)
 		yield full_url
-
-# davon dann full_json getten bis es nicht mehr geht
 
 for url in generate_next_url("https://fragdenstaat.de/api/v1/publicbody/?limit=50&offset="):
 	b = get_full_json(url)
 	if b == "404":
 		print("404. Stopping")
 		break
-
-#generator = generate_next_url("https://fragdenstaat.de/api/v1/publicbody/?limit=50&offset=")
-#next(generator)
-#next(generator)
-#next(generator)
-
-# get_full_json("https://fragdenstaat.de/api/v1/publicbody/")

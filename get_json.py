f760e9d4d6c87f7fd3a2a854f3c3a7933c8a3abc
nportfolio <44119797+chaseinstead@users.noreply.github.com>
2019-05-03 16:54:04 +0200

    included 404 handling

--- get_json.py
+++ get_json.py
@@ -1,18 +1,23 @@
 import requests
 import json
 
+data = []
 
 def get_full_json(url):
 
 	a = requests.get(url)
+
+	if a.status_code == 404:
+		with open('data.json', 'a') as outfile:
+			json.dump(data, outfile)
+
+		return "404"
+	
 	publicbody = a.json()
-	data = []
+	# data = []
 	
 	for p in publicbody["objects"]["results"]:
 		data.append(p)
-
-	with open('data.json', 'w') as outfile:
-		json.dump(data, outfile)
 
 
 def generate_next_url(url):
@@ -21,11 +26,20 @@ def generate_next_url(url):
 	while True:
 		full_url = url + str(number)
 
-		number += 50 
+		number += 50
 
-		yield full_url, number
+#		if number >= 1000:
+#			raise StopIteration()
+
+		yield full_url
 
 # davon dann full_json getten bis es nicht mehr geht
+
+for url in generate_next_url("https://fragdenstaat.de/api/v1/publicbody/?limit=50&offset="):
+	b = get_full_json(url)
+	if b == "404":
+		print("404. Stopping")
+		break
 
 #generator = generate_next_url("https://fragdenstaat.de/api/v1/publicbody/?limit=50&offset=")
 #next(generator)
